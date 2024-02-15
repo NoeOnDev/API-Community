3892db74764828c94c02370ae90dbe447facadcc
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-15 14:35:26 -0600

    Agrega ConfigModule.forRoot() a app.module.ts y utiliza process.env.PORT en main.ts

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/', app, document);
-  await app.listen(3000);
+  const port = process.env.PORT;
+  await app.listen(port);
 }
 bootstrap();
