3d3b4314e2ff38fb58daa359e3ef2666e29cfc16
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-14 23:00:36 -0600

    Agrega soporte para Swagger en la API de NestJS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API REST Example')
+    .setDescription('The NestJS API REST Example')
+    .setVersion('1.0')
+    .addTag('nestjs')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
   await app.listen(3000);
 }
 bootstrap();
