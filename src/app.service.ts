c15a4f93d51fbb0c86ebaa8723c07c03df2b25db
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-16 00:52:25 -0600

    Eliminar archivo de configuración y ajustar comando de inicio de depuración

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  constructor( private configService: ConfigService ) {
-    console.log( this.configService.get('config.port') );
-  }
   getHello(): string {
     return 'Hello World!';
   }
