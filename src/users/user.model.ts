6fdedd9a66b4e485b8c556a329f2802489731bb4
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-17 21:00:18 -0600

    Añadir bcrypt para el cifrado de contraseñas y realizar cambios en el modelo de usuario

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -1,24 +1,51 @@
-import { Column, Table, Model, DataType, Unique } from 'sequelize-typescript';
+import { Column, Table, Model, DataType, Unique, AllowNull, IsEmail, Length, BeforeSave, Index } from 'sequelize-typescript';
+import * as bcrypt from 'bcrypt';
 
-@Table
+@Table({ timestamps: true })
 export class User extends Model {
+    @AllowNull(false)
     @Column
     firstName: string;
 
+    @AllowNull(false)
     @Column
     lastName: string;
 
+    @AllowNull(false)
     @Column(DataType.DATEONLY)
     birthday: Date;
 
     @Unique
     @Column
+    phone: string;
+
+    @Index
+    @Unique
+    @AllowNull(false)
+    @Length({ min: 3, max: 20 })
+    @Column
     username: string;
 
+    @Index
     @Unique
+    @AllowNull(false)
+    @IsEmail
     @Column
     email: string;
 
+    @AllowNull(false)
+    @Length({ min: 8, max: 100 })
     @Column
     password: string;
+
+    @BeforeSave
+    static async hashPassword(user: User) {
+        if (user.changed('password')) {
+            user.password = await bcrypt.hash(user.password, 10);
+        }
+    }
+
+    async checkPassword(password: string) {
+        return await bcrypt.compare(password, this.password);
+    }
 }
