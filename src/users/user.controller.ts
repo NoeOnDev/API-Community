032fbae5f100cf309545f9d05e2caa6d3b6f6545
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-19 00:32:35 -0600

    Agregada funcionalidad de creación de usuarios

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.model';
+import { UserCreateDto } from './dto/user-create.dto';
 
 @Controller('users')
 export class UserController {
@@ -9,5 +10,10 @@ export class UserController {
   @Get()
   async findAll(): Promise<User[]> {
     return this.userService.findAll();
+  }
+
+  @Post()
+  async create(@Body() userCreateDto: UserCreateDto): Promise<User> {
+    return this.userService.create(userCreateDto);
   }
 }
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from './user.model';
+import { UserCreateDto } from './dto/user-create.dto';
 
 @Injectable()
 export class UserService {
@@ -11,5 +12,18 @@ export class UserService {
 
   async findAll(): Promise<User[]> {
     return this.userModel.findAll();
+  }
+
+  async create(userCreateDto: UserCreateDto): Promise<User> {
+    const user = new User();
+    user.firstName = userCreateDto.firstName;
+    user.lastName = userCreateDto.lastName;
+    user.birthday = userCreateDto.birthday;
+    user.phone = userCreateDto.phone;
+    user.username = userCreateDto.username;
+    user.email = userCreateDto.email;
+    user.password = userCreateDto.password;
+
+    return user.save();
   }
 }
