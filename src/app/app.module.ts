7e6e9ce4326cb6171c00311f2fe4d4cd66f36eb3
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-22 09:40:20 -0600

    Eliminado módulo de base de datos y controlador de usuario, y sus dependencias.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,9 @@
-import { UserModule } from '../users/user.module';
-import { DatabaseModule } from '../database/database.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    UserModule,
-    DatabaseModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
