298a85fdfdbdafa87c8dee9b00098fcd53380739
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-02-24 22:44:00 -0600

    Eliminados archivos y dependencias innecesarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { UsersModule } from 'src/users/users.module';
-import { User } from 'src/users/user.model';
 
 @Module({
-  imports: [
-    UsersModule,
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: 'localhost',
-      port: 5434,
-      username: 'root',
-      password: 'password',
-      database: 'testjs',
-      models: [User],
-      autoLoadModels: true,
-      synchronize: true,
-      logging: false,
-    })
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
