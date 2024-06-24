2bbbb036af8bb7d265b000513e029004fec3bbf6
NoeOnDev <alxg5516@gmail.com>
2024-06-23 23:52:10 -0600

    feat: Update server.js to use Express import syntax

---
+++ dist/server.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get('/', (_req, res) => {
+    res.send('Hello World');
+});
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
