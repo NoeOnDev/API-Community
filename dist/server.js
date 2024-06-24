9ad074d79f523d5cbb1a50386aa53ec3a960880c
NoeOnDev <alxg5516@gmail.com>
2024-06-24 00:01:59 -0600

    chore: Update server.js to use Express import syntax

--- dist/server.js
+++ dist/server.js
@@ -4,10 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+process.loadEnvFile();
 const app = (0, express_1.default)();
 app.get('/', (_req, res) => {
     res.send('Hello World');
 });
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from 'express';
 
+process.loadEnvFile();
+
 const app = express();
 
 app.get('/', (_req: Request, res: Response) => {
   res.send('Hello World');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
