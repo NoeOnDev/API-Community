2db11225ca71c534522c58aa82abd33c24730ea2
NoeOnDev <alxg5516@gmail.com>
2024-10-03 23:21:50 -0600

    Refactorizar configuración de variables de entorno

--- dist/index.js
+++ dist/index.js
@@ -5,13 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const verbRoutes_1 = __importDefault(require("./routes/verbRoutes"));
-const env_1 = require("./config/env");
+const morgan_1 = __importDefault(require("morgan"));
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
 const app = (0, express_1.default)();
-const port = env_1.env.port;
+const port = process.env.PORT;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-app.use('/api', verbRoutes_1.default);
+app.use((0, morgan_1.default)('dev'));
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,38 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { config } from "dotenv";
+import { Twilio } from "twilio";
+
+config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
+const twilio = new Twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
+
+app.post("/send-whatsapp", async (req, res) => {
+  try {
+    const { to, message } = req.body;
+    const response = await twilio.messages.create({
+      from: `whatsapp:${process.env.TWILIO_PHONE_NUMBER}`,
+      body: message,
+      to: `whatsapp:${to}`,
+    });
+
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
